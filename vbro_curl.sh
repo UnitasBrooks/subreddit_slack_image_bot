fe78a6ea81078f79b91699cee99c4a19af3bab22
Cloud User <ec2-user@ip-172-31-20-152.us-west-2.compute.internal>
2016-03-06 16:05:13 -0500

    deleting log and random curl script

--- vbro_curl.sh
+++
@@ -1,2 +0,0 @@
-curl --header \"Authorization: Client-ID e8a93eac736a929\" https://api.imgur.com/3/gallery/r/venturebros/time/1
-echo \n curl --header \"Authorization: Client-ID e8a93eac736a929\" https://api.imgur.com/3/gallery/r/venturebros/time/1
